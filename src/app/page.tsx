5c43b1e5eeb24207085ebe78643dc9dcb929ca36
Guilherme Drumond <guilherme.drumond@ciandt.com>
2023-07-05 19:34:37 -0300

    feat: auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
 import React from "react";
 
-
 export default function Home() {
+  const { data } = useSession();
+  console.log(data)
   return (
-    <h1>Teste</h1>
+    <div>
+      <button onClick={()=>signIn()}>Login</button>
+      <h1>{data?.user?.name}</h1>
+    </div>
   )
 }
